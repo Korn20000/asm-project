5e419115a2c823d11f8160fe9c4e222248423bbd
Kornelius Pasztor <korn_20000@hotmail.com>
2018-04-02 18:32:01 +0200

    Indication of start game + switches

--- project/project/main.asm
+++ project/project/main.asm
@@ -47,3 +47,49 @@ badAnswer:
 nextLevel: 
 	// 2 blinking led 3 blinking led ..
 	// stack things.. 
+	
+	
+	/*
+;indication on game startup
+LDI R16, LOW(RAMEND)
+OUT SPL, R16
+LDI R16, HIGH(RAMEND)
+OUT SPH, R16
+
+main:  
+LDI R16, 0xFF  
+OUT DDRA, R16 ; PORTA is output
+
+back:  
+COM R16 ; complement value in R16  
+OUT PORTA, R16 ; push the calue in R16 to PORTB  
+CALL DELAY  
+RJMP back ; infinite loop (should be changed later to a couple of times, and then game start)
+
+delay:  
+LDI R17, 50  
+loop3: LDI R18, 255  
+loop2: LDI R19, 255  
+loop1: DEC R19  
+BRNE loop1 ;keep decreasing R19
+DEC R18  
+BRNE loop2 ; everytime we decrease R18 then we use previous loop
+DEC R17  
+BRNE loop3 ; sane as loop2 instead we decrease R17
+RET        ; return precious address
+*/
+
+/*
+;switches
+Light_all:
+LDI R16, 0x00
+COM R16
+OUT DDRB, R16
+LDI R17, 0xFF
+OUT DDRA, R17
+
+again:
+IN R18, PINB
+OUT PORTA,R18
+JMP again
+*/
